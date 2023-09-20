cb65104658d92f37bbcfd1f9451489976c3d1774
luciendupont <lucienluludupont@gmail.com>
2023-09-20 11:14:55 +0200

    cli

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\Categorie;
+use App\Form\RechercheType;
+use App\Repository\ArticleRepository;
 use App\Repository\CategorieRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
@@ -20,4 +22,5 @@ class IndexController extends AbstractController
         'cats'=>$cat,
         ]);
     }
+
 }
